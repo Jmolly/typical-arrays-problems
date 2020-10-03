71090a844b3ccb5023abb371a75c4e15257c9b2f
jmolly <roshkovskypetr@gmail.com>
2020-10-03 20:46:27 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    let min = 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (array)
+        array.forEach((element) => {
+            min = Math.min(element, min);
+        });
 
-exports.max = function max (array) {
-  return 0;
-}
+    return min;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = 0;
+
+    if (array)
+        array.forEach((element) => {
+            max = Math.max(element, max);
+        });
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    let avg = 0;
+
+    if (array && array.length)
+        avg = array.reduce((acc, red) => acc + red, 0) / array.length;
+
+    return avg;
+};
